e2cf1f0f14c099c5d975b49bad86c858a3a47a6f
Sirius-Development <griffinferguson.us@gmail.com>
2021-10-22 10:33:54 -0500

    Initial commit

---
+++ scripts/intro.js
@@ -0,0 +1,6 @@
+var introElems = {
+    title: document.getElementById('gameTitle')
+};
+function loadIntro() {
+    introElems.title.style.transform = 'translateY(6rem)';
+}
---
+++ scripts/intro.ts
@@ -0,0 +1,8 @@
+var introElems = {
+    wrapper: document.getElementById('intro'),
+    title: document.getElementById('gameTitle')
+}
+
+function loadIntro() {
+    introElems.title.style.transform = 'translateY(6rem)'
+}
