0c89a9ba20d1f082bcf8f5d7e1c8a08c4b3ea81d
Sirius-Development <griffinferguson.us@gmail.com>
2021-10-22 13:19:22 -0500

    update(script): Continue work on script for the intro section

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,6 +1,22 @@
 var introElems = {
-    title: document.getElementById('gameTitle')
+    wrapper: document.getElementById('intro'),
+    title: document.getElementById('gameTitle'),
+    buttonWrapper: document.getElementById('introButtons'),
+    buttons: {
+        play: document.getElementById('playButton'),
+        help: document.getElementById('introHelp'),
+    },
 };
-function loadIntro() {
+function introEnter() {
     introElems.title.style.transform = 'translateY(6rem)';
+    introElems.buttonWrapper.style.transform = 'translateY(50vh)';
 }
+function introExit() {
+    introElems.title.style.transform = 'translateY(-5rem)';
+    introElems.buttonWrapper.style.transform = 'translateY(110vh)';
+}
+introElems.buttons.help.addEventListener('click', function (e) {
+    introExit();
+    showHelp('instructions');
+});
+//# sourceMappingURL=intro.js.map
--- scripts/intro.ts
+++ scripts/intro.ts
@@ -1,8 +1,23 @@
 var introElems = {
     wrapper: document.getElementById('intro'),
-    title: document.getElementById('gameTitle')
+    title: document.getElementById('gameTitle'),
+    buttonWrapper: document.getElementById('introButtons'),
+    buttons: {
+        play: document.getElementById('playButton'),
+        help: document.getElementById('introHelp'),
+    },
 }
 
-function loadIntro() {
+function introEnter() {
     introElems.title.style.transform = 'translateY(6rem)'
+    introElems.buttonWrapper.style.transform = 'translateY(50vh)'
 }
+function introExit() {
+    introElems.title.style.transform = 'translateY(-5rem)'
+    introElems.buttonWrapper.style.transform = 'translateY(110vh)'
+}
+
+introElems.buttons.help.addEventListener('click', function(e) {
+    introExit()
+    showHelp('instructions')
+})
