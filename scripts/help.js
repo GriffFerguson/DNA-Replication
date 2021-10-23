913aa9293129797665fbd84de8559bbd41078f1e
Sirius-Development <griffinferguson.us@gmail.com>
2021-10-23 10:54:06 -0500

    update(scripts/layout): Help overlay now exits correctly

--- scripts/help.js
+++ scripts/help.js
@@ -5,9 +5,17 @@ fetch('/resources/textAssets.json')
     .then(function (json) { textAssets = json; console.log(json); });
 function showHelp(message) {
     helpOverlay.innerHTML = textAssets[message];
-    helpOverlay.style.transform = 'translateY(6vh)';
+    helpOverlay.style.display = 'block';
+    setTimeout(function () {
+        helpOverlay.style.transform = 'translateY(0)';
+    }, 2);
 }
 function hideHelp() {
+    introEnter();
+    window.scrollTo(0, 0);
     helpOverlay.style.transform = 'translateY(110vh)';
+    setTimeout(function () {
+        helpOverlay.style.display = 'none';
+    }, 1510);
 }
 //# sourceMappingURL=help.js.map
--- scripts/help.ts
+++ scripts/help.ts
@@ -7,9 +7,17 @@ fetch('/resources/textAssets.json')
 
 function showHelp(message:string) {
     helpOverlay.innerHTML = textAssets[message]
-    helpOverlay.style.transform = 'translateY(6vh)'
+    helpOverlay.style.display = 'block'
+    setTimeout(() => {
+        helpOverlay.style.transform = 'translateY(0)'
+    }, 2)
 }
 
 function hideHelp() {
+    introEnter()
+    window.scrollTo(0,0)
     helpOverlay.style.transform = 'translateY(110vh)'
+    setTimeout(() => {
+        helpOverlay.style.display = 'none'
+    }, 1510)
 }
